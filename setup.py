44ab55b3ecb84071d5c29ea8b1708f5372317595
ManishankarRJ <shankarrajasekaran2000rj@gmail.com>
2021-03-25 13:05:15 +0530

    Hello

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='OPD CARE',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='SHAN',
+    author_email='Example@gmail.com',
+    description='OPD CARE for college python course'
+)
